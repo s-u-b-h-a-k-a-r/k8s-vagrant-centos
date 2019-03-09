20e7a1b30242c48a67955eb9d9fa51cd4f3ed9f9
SubhakarKotta <subhakarkotta@gmail.com>
2019-03-09 12:16:07 +0530

    k8s-vagrant-centos: nfs-provisioner default for persistence

--- provisioning/vm-worker/install.sh
+++ provisioning/vm-worker/install.sh
@@ -2,18 +2,30 @@
 
 figlet WORKER
 
-echo "[TASK 1] Create /mnt/fast-disks Directory For Local Volume Provisioning"
-mkdir /mnt/fast-disks
-chmod 777 -R /mnt/fast-disks
-for i in `seq 1 200`;
-	do
-		DIRNAME="vol$i"
-		mkdir -p /mnt/fast-disks/$DIRNAME 
-		chcon -Rt svirt_sandbox_file_t /mnt/fast-disks/$DIRNAME
-		chmod 777 /mnt/fast-disks/$DIRNAME
-        mount -t tmpfs -o size=10G $DIRNAME /mnt/fast-disks/$DIRNAME
-		echo "created directory $DIRNAME"
-done
+#echo "[TASK 1] Create /mnt/fast-disks Directory For Local Volume Provisioning"
+#mkdir /mnt/fast-disks
+#chmod 777 -R /mnt/fast-disks
+#for i in `seq 1 20`;
+#	do
+#		DIRNAME="vol$i"
+#		mkdir -p /mnt/fast-disks/$DIRNAME 
+#		chcon -Rt svirt_sandbox_file_t /mnt/fast-disks/$DIRNAME
+#		chmod 777 /mnt/fast-disks/$DIRNAME
+#       mount -t tmpfs -o size=10G $DIRNAME /mnt/fast-disks/$DIRNAME
+#		echo "created directory $DIRNAME"
+#done
+
+
+#for i in `seq 21 40`;
+#	do
+#		DIRNAME="vol$i"
+#		mkdir -p /mnt/fast-disks/$DIRNAME 
+#		chcon -Rt svirt_sandbox_file_t /mnt/fast-disks/$DIRNAME
+#		chmod 777 /mnt/fast-disks/$DIRNAME
+ #       mount -t tmpfs -o size=5G $DIRNAME /mnt/fast-disks/$DIRNAME
+#		echo "created directory $DIRNAME"
+#done
+
 
 # Join Worker Nodes To The Kubernetes Cluster
 echo "[TASK 2] Join Node To Kubernetes Cluster"
